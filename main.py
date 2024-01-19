f25469a7a5e1c1d6c1fdb93be94058fc2f15166e
Odame <kwesi.danquah@daaseng.com>
2024-01-19 11:38:53 +0000

    Employee Salary Prediction

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-# This is a sample Python script.
+# import libraries
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 
+df = pd.read_csv("data/salaries-2023.csv")
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print(df.head())
